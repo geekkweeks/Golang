e04982a4b8d0f2711492585280939b0adcf8f3e2
geekkweek <zahgreat30@gmail.com>
2022-07-24 09:37:39 +0700

    advance return multiple value

--- Function/return-multiple-value.go
+++ Function/return-multiple-value.go
@@ -8,9 +8,19 @@ func detailInfo(name string, salary int, isPermanent bool) (string, int, bool) {
 	return name, salary, isPermanent
 }
 
+func fullName() (string, string, string) {
+	return "Alfan", "Zahri", "Yono"
+}
+
 func main() {
 	name, salary, isPermanent := detailInfo("Alfan", 500000, true)
 	fmt.Println(name)
 	fmt.Println(salary)
 	fmt.Println(isPermanent)
+
+	firstName, _, _ := fullName()
+	fmt.Println(firstName)
+
+	_, middleName, _ := fullName()
+	fmt.Println(middleName)
 }
