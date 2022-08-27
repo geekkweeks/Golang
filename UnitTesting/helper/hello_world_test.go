c9c5b49c6f9fe4f5da2d63f77472e01ae011f57d
geekkweek <zahgreat30@gmail.com>
2022-08-27 09:54:45 +0700

    error by using Fatal and FatalNow

--- UnitTesting/helper/hello_world_test.go
+++ UnitTesting/helper/hello_world_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	name := "Alfan"
@@ -16,4 +19,23 @@ func TestHelloWorldEmployee(t *testing.T) {
 	if result != "Hello "+name {
 		panic("Result is not same")
 	}
+}
+
+//failed testing. Not recommened to using Panic function to define failed inside of unit testing
+func TestHelloWorldFailed(t *testing.T) {
+	name := "Yono"
+	result := HelloWorld(name)
+	if result != "HI "+name {
+		t.Fail()
+	}
+	fmt.Println("Keep executed even the unit test was failed by using t.Fail()")
+}
+
+func TestHelloWorldFailedNo(t *testing.T) {
+	name := "Yono"
+	result := HelloWorld(name)
+	if result != "HI "+name {
+		t.FailNow()
+	}
+	fmt.Println("Will not executed")
 }
