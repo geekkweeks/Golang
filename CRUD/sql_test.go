c5cde9ffe5325942aec85587473efdeecefe07ab
geekkweek <zahgreat30@gmail.com>
2022-12-30 00:03:12 +0700

    SQL with param

--- CRUD/sql_test.go
+++ CRUD/sql_test.go
@@ -1,8 +1,8 @@
 package music_crud
 
 import (
-	"database/sql"
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
 )
@@ -40,6 +40,38 @@ func TestExecSelectSql(t *testing.T) {
 	// iterate data from return of query select
 	// if rows.Next = false iterate will stop
 	for rows.Next() {
+		var id int
+
+		// to handle nullable from SQL, using sql.NullString
+		var name sql.NullString
+
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("ID: ", id)
+		fmt.Println("Name: ", name)
+	}
+}
+
+// To avoid SQL injection, the best way is execute the SQL by using paramter
+func TestExecSelectWithParamSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	var paramId int = 1
+	sqlString := "SELECT ID, NAME FROM ARTISTS WHERE ID = $1"
+	rows, err := db.QueryContext(ctx, sqlString, paramId)
+	if err != nil {
+		panic(err)
+	}
+	// dont forget to always close row
+	defer rows.Close()
+
+	// if rows.Next = false iterate will stop
+	if rows.Next() {
 		var id int
 
 		// to handle nullable from SQL, using sql.NullString
