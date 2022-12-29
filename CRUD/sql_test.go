afb71a5022c0da081b57e58c995c27f21a442157
geekkweek <zahgreat30@gmail.com>
2022-12-29 23:37:23 +0700

    execute SQL select

--- CRUD/sql_test.go
+++ CRUD/sql_test.go
@@ -6,13 +6,13 @@ import (
 	"testing"
 )
 
-func TestExecSqlQuery(t *testing.T) {
+func TestExecInsertSql(t *testing.T) {
 	db := GetConnection()
 	defer db.Close()
 
 	ctx := context.Background()
 
-	sqlString := "INSERT INTO ARTISTS(ID,NAME) VALUES(2, 'ZAHRIYONO')"
+	sqlString := "INSERT INTO ARTISTS(ID,NAME) VALUES(4, 'Gulung')"
 	_, err := db.ExecContext(ctx, sqlString)
 	if err != nil {
 		panic(err)
@@ -20,4 +20,32 @@ func TestExecSqlQuery(t *testing.T) {
 
 	fmt.Println("Data has been inserted")
 
+}
+
+func TestExecSelectSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	sqlString := "SELECT ID, NAME FROM ARTISTS"
+	rows, err := db.QueryContext(ctx, sqlString)
+	if err != nil {
+		panic(err)
+	}
+	// dont forget to always close row
+	defer rows.Close()
+
+	// iterate data from return of query select
+	// if rows.Next = false iterate will stop
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("ID: ", id)
+		fmt.Println("Name: ", name)
+	}
 }
