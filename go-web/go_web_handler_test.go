eb57e344595c76886621c9da3c7131a5cef842d8
geekkweek <zahgreat30@gmail.com>
2023-01-02 15:58:40 +0700

    Add handler for web

---
+++ go-web/go_web_handler_test.go
@@ -0,0 +1,25 @@
+package go_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGoWebHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		// logic web
+		fmt.Fprint(writer, "Hello Golang Developer in the world!")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
+}
