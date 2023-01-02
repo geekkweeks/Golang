5bf0b2fbb77e7e123e243b542eb0bef7c9e88a63
geekkweek <zahgreat30@gmail.com>
2023-01-02 16:24:07 +0700

    get http request information from client

---
+++ go-web/go_web_http_request_test.go
@@ -0,0 +1,24 @@
+package go_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestRequestClient(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, request.Method)
+		fmt.Fprintln(writer, request.RequestURI)
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
