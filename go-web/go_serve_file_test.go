337f39bbb17d9a88211e058a7c1447179ad55161
geekkweek <zahgreat30@gmail.com>
2023-01-16 20:11:59 +0700

    embed file

--- go-web/go_serve_file_test.go
+++ go-web/go_serve_file_test.go
@@ -1,9 +1,18 @@
 package go_web
 
 import (
+	"fmt"
+	_ "embed"
 	"net/http"
 	"testing"	
 )
+
+//go:embed resources/ok.html
+var resourceOk string
+
+//go:embed resources/not_found.html
+var resourceNotFound string
+
 
 func ServeFile(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Query().Get("name") != "" {
@@ -13,10 +22,30 @@ func ServeFile(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourceOk)
+	}else{
+		fmt.Fprint(w, resourceNotFound)
+	}
+}
+
 func TestServeFile(t *testing.T) {
 	server := http.Server{
 		Addr: "localhost:8080",
 		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
 	}
 
 	err := server.ListenAndServe()
