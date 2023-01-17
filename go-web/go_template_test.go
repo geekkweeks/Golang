17d1cbe9f6a4eedff36f126c06f5e6c97143a61e
geekkweek <zahgreat30@gmail.com>
2023-01-17 11:25:30 +0700

    go template

---
+++ go-web/go_template_test.go
@@ -0,0 +1,61 @@
+package go_web
+
+import (
+	_ "embed"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SimpleHtmlFile(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/simple.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello great developer")
+}
+
+func TemplateDirectory(w http.ResponseWriter, r *http.Request) {
+	// looking for all file with .gohtml extension
+	t := template.Must(template.ParseGlob("./templates/*.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Hi ALL")
+}
+
+//go.embed templates/*.gohtml
+var templates string embed.FS
+
+func TemplateEmbed(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFS(templates, "templates/*gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Hayooo")
+}
+
+
+func TestSimpleHtmlFile(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHtmlFile(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(body)
+}
+
+func TestTemplateDirectory(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDirectory(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(body)
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateEmbed(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(body)
+}
