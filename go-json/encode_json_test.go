843dbf70bebe4dc6b78d68d3da82b489f4346961
geekkweek <zahgreat30@gmail.com>
2023-01-20 21:30:36 +0700

    encode Json Object

--- go-json/encode_json_test.go
+++ go-json/encode_json_test.go
@@ -6,6 +6,14 @@ import (
 	"testing"
 )
 
+type Employee struct {
+	Id          int
+	Name        string
+	Department  string
+	Salary      int
+	IsPermanent bool
+}
+
 func logJson(data interface{}) {
 	bytes, err := json.Marshal(data)
 	if err != nil {
@@ -19,4 +27,14 @@ func TestLogJson(t *testing.T) {
 	logJson(23)
 	logJson(true)
 	logJson([]string{"MTK", "BAHASA", "IPS"})
+
+	//encode object json
+	employeeObj := Employee{
+		Id:          23,
+		Name:        "Alfan Zahriyono",
+		Department:  "IT",
+		Salary:      23000,
+		IsPermanent: true,
+	}
+	logJson(employeeObj)
 }
